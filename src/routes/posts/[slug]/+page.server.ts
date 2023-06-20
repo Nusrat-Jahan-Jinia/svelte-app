df95c3888c88364a9d77f23e22304ad8bc5c5701
nusrat <nusratcode14@gmail.com>
2023-06-21 02:16:35 +0600

    removed error code

--- src/routes/posts/[slug]/+page.server.ts
+++ src/routes/posts/[slug]/+page.server.ts
@@ -18,20 +18,3 @@ type Data = {
 	success: boolean;
 	errors: Record<string, string>;
 };
-export const POST: RequestHandler = async ({ request }) => {
-	const formData = await request.formData();
-	const post = String(formData.get('post'));
-
-	const data: Data = {
-		success: false,
-		errors: {}
-	};
-
-	if (!post) {
-		data.errors.post = 'required';
-		return json(data, { status: 400 });
-	}
-	addPost(post);
-	data.success = true;
-	return json(data);
-};
